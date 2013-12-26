eef8f88c27ce429d89e7689eb6d42baf234f76b6
smiley22 <smileytwentytwo@gmail.com>
2013-12-26 20:45:31 +0100

    Initial Commit

---
+++ Extensions/IOutputFilter.cs
@@ -0,0 +1,15 @@
+﻿using S22.Xmpp.Core;
+
+namespace S22.Xmpp.Extensions {
+	/// <summary>
+	/// Represents an extension that filters outgoing stanzas.
+	/// </summary>
+	/// <typeparam name="T">The type of the stanzas to filter.</typeparam>
+	internal interface IOutputFilter<T> where T : Stanza {
+		/// <summary>
+		/// Invoked when a stanza is being sent.
+		/// </summary>
+		/// <param name="stanza">The stanza which is being sent.</param>
+		void Output(T stanza);
+	}
+}
---
+++ Extensions/XEP-0077/RegistrationCallback.cs
@@ -0,0 +1,14 @@
+﻿using S22.Xmpp.Extensions.Dataforms;
+
+namespace S22.Xmpp.Extensions {
+	/// <summary>
+	/// A callback method which is invoked during the registration process to
+	/// let the user fill out the required fields needed to complete the
+	/// registration.
+	/// </summary>
+	/// <param name="form">A request form containing the fields to be filled
+	/// out.</param>
+	/// <returns>An initialized instance of the SubmitForm class containing the
+	/// filled-out fields.</returns>
+	public delegate SubmitForm RegistrationCallback(RequestForm form);
+}
